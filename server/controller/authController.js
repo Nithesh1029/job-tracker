52d41d67c2af944e513150d20e2ae89cbe84ad37
Nithesh <niteshpoojary1029@gmail.com>
2026-06-07 14:36:34 +0530

    .

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -50,7 +50,7 @@ export const login =async(req ,res)=>{
         res.cookie('token',token,{
             httpOnly:true,
             secure: process.env.NODE_ENV === "production",
-            sameSite:'strict',
+            sameSite:"none",
             maxAge:3600000
         });
         const user = {
@@ -73,7 +73,7 @@ export const logOut=async(req,res)=>{
         res.clearCookie('token',{
             httpOnly:true,
             secure:process.env.NODE_ENV==="production",
-            sameSite:"strict"
+            sameSite:"none"
         });
         res.status(200).json({message:"Logged out successfully"});
     } catch (error) {
